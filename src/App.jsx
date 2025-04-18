c2800286c9bc899d884a88b8aa8a79ba791b16a1
Adarsh Shukla <150530437+shuklaAdarsh2608@users.noreply.github.com>
2025-04-18 09:07:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AboutMain from './components/aboutme/AboutMain'
 import HelperSection from './components/HelperSection'
 import SkillsMain from './components/skillsSection/SkillsMain'
 import SubSkills from './components/skillsSection/SubSkills'
-import ExperienceMAin from './components/experience/ExperienceMAin'
+import ExperienceMain from './components/experience/ExperienceMain'
 import ProjectMain from './components/projects/ProjectMain'
 import ContactMeMain from './components/contactMe/ContactMeMain'
 import FooterM from './components/footer/FooterM'
@@ -22,7 +22,7 @@ function App() {
     <AboutMain/>
     <SkillsMain/>
     <SubSkills/>
-    <ExperienceMAin/>
+    <ExperienceMain/>
     <ProjectMain/>
     <ContactMeMain/>
     <FooterM/>
